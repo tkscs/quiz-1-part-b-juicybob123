02aff733c5c5dbd67e940088f3da1dbbc1e7e834
juicybob123 <okruh2028@kehillahstudent.org>
2025-11-06 12:03:22 -0800

    done

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import turtle
-
+from turtle import *
 # YOUR CODE HERE
+for i in range (4):
+    forward(100)
+    right(90)
+penup
+forward(50)
+pendown
+right(45)
+for i in range(4):
+    forward(71)
+    right(90)
 
 turtle.exitonclick()
